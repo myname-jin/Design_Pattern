73affd2bc5868685c684249b751cf7a9d1f76329
myname-jin <adsd3@example.com>
2025-10-01 13:46:08 +0900

    test

---
+++ Design_Pattern_Project/src/main/java/com/mycompany/design_pattern_project/Design_Pattern_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.design_pattern_project;
+
+/**
+ *
+ * @author adsd3
+ */
+public class Design_Pattern_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Woasdrld!");
+    }
+}
