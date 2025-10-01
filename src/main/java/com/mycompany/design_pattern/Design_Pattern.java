98c1eb4e78af7c62253d4bdd24d25b0e01df0327
myname-jin <adsd3@example.com>
2025-10-01 13:39:49 +0900

    Initial commit

---
+++ src/main/java/com/mycompany/design_pattern/Design_Pattern.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.design_pattern;
+
+/**
+ *
+ * @author adsd3
+ */
+public class Design_Pattern {
+
+    public static void main(String[] args) {
+        System.out.println("Helasaasdlo World!");
+    }
+}
